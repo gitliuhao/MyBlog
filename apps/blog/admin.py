9a30f8deabd381f5a56b21bc0473ba9c7f39f5e7
root <1020193131@qq.com>
2018-05-06 19:00:05 +0800

    update

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import options
+
+options
 
 # Register your models here.
 from django_manager import admin as manager_admin
-manager_admin.registers(editor_md_fields=('change_message',))
+manager_admin.registers(editor_md_fields=('body',))
+
--- apps/blog/tests.py
+++ apps/blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from sys import getrefcount
+a = ['x']
+print(getrefcount(a))
+def p(x):
+    print(getrefcount(a))
+    b = x
+    print(getrefcount(a))
+    # del a
+    # del b
+    # del x
+    return b
+p(a)
+print(getrefcount(a))
+p(a)
+print(getrefcount(a))
+b = a
+print(getrefcount(a))
+del a
+
